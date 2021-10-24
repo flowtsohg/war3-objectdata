bf506f33f954a16ffd2fcb40126e61a0349ccaf5
Chananya Freiman <spectralcanine@gmail.com>
2021-10-24 19:30:10 +0300

    git is ignoring my gitignore

--- dist/cjs/index.d.ts
+++
@@ -1,5 +0,0 @@
-export { ObjectData } from './objectdata';
-export { Unit, Units } from './generated/units';
-export { Item, Items } from './generated/items';
-export { Destructable, Destructables } from './generated/destructables';
-export { Doodad, Doodads } from './generated/doodads';
