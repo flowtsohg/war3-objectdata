ef74ed73320e01c5eb618dc6465b4d44b3dd6c28
trigger <triggerhappy187@live.com>
2023-05-12 15:17:45 -0700

    1.33 update (thanks ttay)

--- src/container.ts
+++ src/container.ts
@@ -1,7 +1,7 @@
-import Modification from 'mdx-m3-viewer/dist/cjs/parsers/w3x/w3u/modification';
-import ModificationTable from 'mdx-m3-viewer/dist/cjs/parsers/w3x/w3u/modificationtable';
-import ModifiedObject from 'mdx-m3-viewer/dist/cjs/parsers/w3x/w3u/modifiedobject';
-import { generateId, pushArray, tsToWar3, war3ToTS } from './utils';
+import Modification from "mdx-m3-viewer/dist/cjs/parsers/w3x/w3u/modification";
+import ModificationTable from "mdx-m3-viewer/dist/cjs/parsers/w3x/w3u/modificationtable";
+import ModifiedObject from "mdx-m3-viewer/dist/cjs/parsers/w3x/w3u/modifiedobject";
+import { generateId, pushArray, tsToWar3, war3ToTS } from "./utils";
 
 export interface IDs {
   oldId: string;
@@ -9,8 +9,8 @@ export interface IDs {
 }
 
 interface Objects<T> {
-  game: Readonly<{[key: string]: Readonly<T> }>;
-  map: {[key: string]: T };
+  game: Readonly<{ [key: string]: Readonly<T> }>;
+  map: { [key: string]: T };
 }
 
 interface Prop {
@@ -29,7 +29,12 @@ function getProp(id: string, props: Prop[]): Prop | undefined {
   return;
 }
 
-export function objectLoader<T extends IDs>(object: T, modifications: Modification[], props: Prop[], specificProps?: {[key: string]: Prop[] }) {
+export function objectLoader<T extends IDs>(
+  object: T,
+  modifications: Modification[],
+  props: Prop[],
+  specificProps?: { [key: string]: Prop[] }
+) {
   for (const modification of modifications) {
     const { id, value } = modification;
     let prop = getProp(id, props);
@@ -45,7 +50,9 @@ export function objectLoader<T extends IDs>(object: T, modifications: Modificati
     }
 
     if (!prop) {
-      throw Error(`Failed to get an Ability prop: ${object.oldId} ${id}`);
+      throw Error(
+        `Failed to get an Ability prop: ${object.oldId}:${object.newId} => ${id}`
+      );
     }
 
     // A neccessary evil.
@@ -53,12 +60,19 @@ export function objectLoader<T extends IDs>(object: T, modifications: Modificati
   }
 }
 
-export function objectSaver<T extends IDs>(gameObject: T, object: T, baseProps: Prop[], specificProps?: {[key: string]: Prop[] }): Modification[] {
+export function objectSaver<T extends IDs>(
+  gameObject: T,
+  object: T,
+  baseProps: Prop[],
+  specificProps?: { [key: string]: Prop[] }
+): Modification[] {
   const modifications = [];
 
   for (const prop of baseProps) {
     if ((<any>gameObject)[prop.name] !== (<any>object)[prop.name]) {
-      modifications.push(tsToWar3(prop.id, prop.type, (<any>object)[prop.name]));
+      modifications.push(
+        tsToWar3(prop.id, prop.type, (<any>object)[prop.name])
+      );
     }
   }
 
@@ -68,7 +82,9 @@ export function objectSaver<T extends IDs>(gameObject: T, object: T, baseProps: 
     if (props) {
       for (const prop of props) {
         if ((<any>gameObject)[prop.name] !== (<any>object)[prop.name]) {
-          modifications.push(tsToWar3(prop.id, prop.type, (<any>object)[prop.name]));
+          modifications.push(
+            tsToWar3(prop.id, prop.type, (<any>object)[prop.name])
+          );
         }
       }
     }
@@ -77,7 +93,14 @@ export function objectSaver<T extends IDs>(gameObject: T, object: T, baseProps: 
   return modifications;
 }
 
-function loadObject<T extends IDs, E>(objects: Objects<T>, oldId: string, newId: string, modifications: Modification[], props: Prop[], specificProps?: {[key: string]: Prop[] }) {
+function loadObject<T extends IDs, E>(
+  objects: Objects<T>,
+  oldId: string,
+  newId: string,
+  modifications: Modification[],
+  props: Prop[],
+  specificProps?: { [key: string]: Prop[] }
+) {
   let objectId;
 
   if (newId === "\0\0\0\0") {
@@ -92,14 +115,22 @@ function loadObject<T extends IDs, E>(objects: Objects<T>, oldId: string, newId:
     throw Error(`Failed to load an object: ${oldId}`);
   }
 
-  const mapObject = Object.seal(Object.assign({}, { ...gameObject, oldId, newId }));
+  const mapObject = Object.seal(
+    Object.assign({}, { ...gameObject, oldId, newId })
+  );
 
   objectLoader(mapObject, modifications, props, specificProps);
 
   objects.map[objectId] = mapObject;
 }
 
-export function load<T extends IDs, E>(objects: Objects<T>, originalTable: ModificationTable, customTable: ModificationTable, props: Prop[], specificProps?: {[key: string]: Prop[] }): void {
+export function load<T extends IDs, E>(
+  objects: Objects<T>,
+  originalTable: ModificationTable,
+  customTable: ModificationTable,
+  props: Prop[],
+  specificProps?: { [key: string]: Prop[] }
+): void {
   for (const { oldId, newId, modifications } of originalTable.objects) {
     loadObject(objects, oldId, newId, modifications, props, specificProps);
   }
@@ -109,7 +140,11 @@ export function load<T extends IDs, E>(objects: Objects<T>, originalTable: Modif
   }
 }
 
-export function save<T extends IDs, E>(objects: Objects<T>, props: Prop[], specificProps?: {[key: string]: Prop[] }): { original: ModifiedObject[], custom: ModifiedObject[] } {
+export function save<T extends IDs, E>(
+  objects: Objects<T>,
+  props: Prop[],
+  specificProps?: { [key: string]: Prop[] }
+): { original: ModifiedObject[]; custom: ModifiedObject[] } {
   const original: ModifiedObject[] = [];
   const custom: ModifiedObject[] = [];
 
@@ -117,7 +152,9 @@ export function save<T extends IDs, E>(objects: Objects<T>, props: Prop[], speci
     const gameObject = objects.game[object.oldId];
 
     if (!gameObject) {
-      throw Error(`Tried to save the modifications of an object with an invalid oldId: ${object.oldId} (newId=${object.newId})`);
+      throw Error(
+        `Tried to save the modifications of an object with an invalid oldId: ${object.oldId} (newId=${object.newId})`
+      );
     }
 
     const modifications = objectSaver(gameObject, object, props, specificProps);
@@ -127,7 +164,7 @@ export function save<T extends IDs, E>(objects: Objects<T>, props: Prop[], speci
 
       modifiedObject.oldId = object.oldId;
       modifiedObject.newId = object.newId;
-      pushArray(modifiedObject.modifications, modifications)
+      pushArray(modifiedObject.modifications, modifications);
 
       if (object.newId === "\0\0\0\0") {
         original.push(modifiedObject);
@@ -144,27 +181,29 @@ export abstract class Container<T extends IDs> {
   /**
    * Game objects.
    */
-  abstract game: Readonly<{[key: string]: Readonly<T>}>;
-  
+  abstract game: Readonly<{ [key: string]: Readonly<T> }>;
+
   /**
    * Map objects.
    */
-  map: {[key: string]: T } = {};
+  map: { [key: string]: T } = {};
 
   /**
    * Get an existing object.
-   * 
+   *
    * If the object isn't in the map data but is in the game data, it will be copied to the map data.
    */
   get(id: string): T | undefined {
     let object = this.map[id];
-    
+
     if (!object) {
       const gameObject = this.game[id];
 
       // If this object exists in the game data, copy it and add it to the map data in case it is modified by the caller.
       if (gameObject) {
-        object = Object.seal(Object.assign({}, { ...gameObject, oldId: id, newId: "\0\0\0\0" }));
+        object = Object.seal(
+          Object.assign({}, { ...gameObject, oldId: id, newId: "\0\0\0\0" })
+        );
 
         this.map[id] = object;
       }
@@ -175,23 +214,23 @@ export abstract class Container<T extends IDs> {
 
   /**
    * Copy an existing object.
-   * 
+   *
    * The source object can either be given as a string ID, or an object returned from previous get/copy calls.
-   * 
+   *
    * If newId is supplied, it will be used as the new object's ID, otherwise a random ID is generated.
-   * 
+   *
    * If a random ID is generated, its first letter will be capitalized if the base ID's first letter is capitalized, to support hero units.
    */
   copy(baseIdOrObject: string | T, newId?: string): T | undefined {
     let baseId: string;
     let baseObject;
 
-    if (typeof baseIdOrObject === 'string') {
+    if (typeof baseIdOrObject === "string") {
       baseId = baseIdOrObject;
 
       // Is this object from the map?
       baseObject = this.map[baseId];
-      
+
       // Is this object from the game?
       if (!baseObject) {
         baseObject = this.game[baseId];
@@ -202,7 +241,7 @@ export abstract class Container<T extends IDs> {
     }
 
     if (!baseObject) {
-      throw Error(`Tried to copy an object that does not exist: ${baseId}`)
+      throw Error(`Tried to copy an object that does not exist: ${baseId}`);
     }
 
     // If an ID was't given, generate one.
@@ -211,7 +250,9 @@ export abstract class Container<T extends IDs> {
     }
 
     // Copy the object.
-    const object = Object.seal(Object.assign({}, baseObject, { oldId: baseId, newId}));
+    const object = Object.seal(
+      Object.assign({}, baseObject, { oldId: baseId, newId })
+    );
 
     this.map[newId] = object;
 
@@ -220,7 +261,7 @@ export abstract class Container<T extends IDs> {
 
   /**
    * Checks if the map contains an buff with the given ID.
-   * 
+   *
    * Does not the game data.
    */
   has(id: string): boolean {
